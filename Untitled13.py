fbe5db034e42313142a5612755e704276533c4d0
zuhren <149810430+zuhren@users.noreply.github.com>
2023-11-08 17:13:37 +0300

    Add files via upload

---
+++ Untitled13.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n = int(input("Pozitif bir üst değer giriniz: "))
+
+a = 0
+b = 1
+while True : 
+    c = a + b
+    a = b
+    b = c
+    if c<= n:
+        print(c)
+    else:
+        break
+
