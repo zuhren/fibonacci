c59044c4883dc44a868ed71906cd67b06abe86f4
zuhren <149810430+zuhren@users.noreply.github.com>
2023-11-11 14:04:16 +0300

    Update kisi secme random.py

--- kisi secme random.py
+++ kisi secme random.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[67]:
-
-
 import random
 
+lst = ["sam", "dean", "jo", "sarah", "jones", "matthew", "samuel", "ted", "barney", "robin", "marshall", "lily", "joanna"]
 
-lst = ["sam", "dean", "jo", "sarah", "jones", "matthew", "samuel", "ted", "barney", "robin", "marshall", "lily", "joanna", "noah"]
 while lst:
     chosen = random.sample(lst,2)
     print(chosen)
     
     for item in chosen:
         lst.remove(item)
+    if len(lst) % 2 == 1:
+        lst.append("   ")
     
+
 
